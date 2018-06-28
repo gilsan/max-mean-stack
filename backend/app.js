79e7f25d0c21a937ce43aceef5da443d07916252
han <jhhan@allaboutsip.co.kr>
2018-06-28 13:24:39 +0900

    mongodb add

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extened: false}));
@@ -18,31 +20,42 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose.connect('mongodb://localhost/mean-stack')
+  .then(  () => { console.log('접속 성공......'); })
+  .catch( () => { console.log('접속 실패....'); })
+
 app.post("/api/posts",(req, res, next) => {
-   const post = req.body;
-   console.log(post);
-   res.status(200).json({
-     message: 'Post added successfully'
+   const post = new Post({
+     title: req.body.title,
+     content: req.body.content
+   });
+   post.save().then( (createdPost) => {
+    console.log('데이터 저장: ',createdPost);
+    res.status(200).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
    })
+
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "서버 사이드 포스트 첫번째",
-      content: "서버에서 보내온 것입니다."
-    },
-    {
-      id: "ksajflaj132",
-      title: "서버 사이드 포스트 두번째",
-      content: "서버에서 알려준 정보!"
-    }
-  ];
-  res.status(200).json({
-    message: "서버에서 데이타 가져오기 성공!",
-    posts: posts
+  Post.find().then( document => {
+    res.status(200).json({
+      message: "서버에서 데이타 가져오기 성공!",
+      posts: document
+    });
   });
 });
+
+app.delete("/api/posts/:id", (req,res,next) => {
+   console.log(req.params.id);
+   Post.deleteOne({_id: req.params.id}).then(result => {
+     console.log(result);
+    res.status(200).json( { message: " 데이타가 삭제 되었습니다..."});
+   })
+
+});
+
 
 module.exports = app;
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,11 +11,7 @@ import { PostsService } from '../../services/posts.service';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  // posts = [
-  //   { title: "First Post", content: "This is the first post's content" },
-  //   { title: "Second Post", content: "This is the second post's content" },
-  //   { title: "Third Post", content: "This is the third post's content" }
-  // ];
+
    posts: Post[] = [];
    private postsSub: Subscription;
   posts$: Observable<Post[]>;
@@ -24,10 +20,17 @@ export class PostListComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.postsService.getPosts();
     this.postsSub =  this.postsService.postsUpdate$
+  // this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe( (posts: Post[]) => {
         this.posts = posts;
       }  );
   }
+
+  onDelete(postId: string) {
+  // console.log('post-list: ', postId);
+    this.postsService.deletePost(postId);
+  }
+
 
   ngOnDestroy() {
    this.postsSub.unsubscribe();
