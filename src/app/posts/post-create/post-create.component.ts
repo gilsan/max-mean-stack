111037bf7c2857956b463af6378b66f5145fb327
gilsan <jhhan@allaboutsip.co.kr>
2018-06-30 14:46:21 +0900

    blob

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
 import { Component , OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {   FormBuilder, FormGroup, Validators,  } from '@angular/forms';
 import { Post } from '../../shared/post.model';
 import { PostsService } from '../../services/posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -14,20 +15,29 @@ export class PostCreateComponent implements OnInit {
    private mode = 'create';
    private postId: string;
     post: Post;
+    form: FormGroup;
+    imagePreview: string;
+
   constructor(
     private postsService:  PostsService,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
+    this.buildForm();
     this.router.paramMap.subscribe( (paramMap: ParamMap) => {
+
       if (paramMap.has('postId')) {
           this.mode = 'edit';
           this.postId = paramMap.get('postId');
           this.postsService.getPost(this.postId)
             .subscribe( postData => {
-               this.post = {id: postData._id, title: postData.title, content: postData.content }
+
+               this.post = {id: postData._id, title: postData.title, content: postData.content };
+               this.form.setValue({'title': this.post.title, 'content': this.post.content});
             });
+
         } else {
          this.mode = 'create';
          this.postId = null;
@@ -35,21 +45,45 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  buildForm() {
+
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      content: ['', [Validators.minLength(3)]],
+      image: ['', [ Validators.required],  mimeType.bind(this) ]
+    });
+
+  }
+  // 그림올리기와 보기
+  onImagePicked(event: Event) {
+    const file = ( event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+   reader.onload = () => {
+     this.imagePreview = reader.result;
+
+   };
+   reader.readAsDataURL(file);
+
+  }
+
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
 
     if (this.mode === 'create') {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
      this.postsService.updatePost(
        this.postId,
-       form.value.title,
-       form.value.content
+       this.form.value.title,
+       this.form.value.content
      );
     }
 
-    form.resetForm();
+    this.form.reset();
   }
 }
